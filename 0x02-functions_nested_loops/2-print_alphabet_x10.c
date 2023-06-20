38ded4ebd2b49acb9f58e337884a1e7ba04bc222
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-20 06:26:20 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void
+ */
+void	print_alphabet_x10(void)
+{
+	int	i, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
