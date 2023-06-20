9450283e5796c7fbff6f602cfbd03285ccb24286
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-20 06:20:50 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * return - void
+*/
+void	print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
