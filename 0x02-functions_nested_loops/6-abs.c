6940a19d02ecf4b355565baa4631882362d644a1
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-20 06:41:08 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @m: integer to be computed
+ *
+ * Return: absolute value of n
+*/
+int	_abs(int m)
+{
+	return ((m < 0) ? -m : m);
+}
