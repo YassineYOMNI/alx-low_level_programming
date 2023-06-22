15854adb7fe6f050e0fe5489111e601eaa360b17
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-22 06:24:40 -0700

    c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b <= 14; b++)
+	{
+	if (b > 9)
+	{
+	putchar((b / 10) + '0');
+	}
+	putchar((b % 10) + '0');
+	}
+	putchar(10);
+	}
+}
