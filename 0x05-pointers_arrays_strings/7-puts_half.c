747d292a9e652c166432b14c708184fd10549045
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-26 07:43:57 -0700

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string to be printed
+ */
+void puts_half(char *str)
+{
+	int str_len = 0;
+	int i = 0;
+
+	while (str[str_len])
+		str_len++;
+
+	i = (str_len % 2 == 0) ? str_len / 2 : (str_len + 1) / 2;
+
+	while (str[i])
+		_putchar(str[i++]);
+	_putchar('\n');
+}
