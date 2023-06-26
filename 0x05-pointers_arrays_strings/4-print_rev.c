2d97ef81dc5c9ff0fe6262175d4e3cef87cec17a
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-26 07:14:08 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
