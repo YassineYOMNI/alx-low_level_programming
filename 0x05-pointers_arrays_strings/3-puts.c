ba0fb652e51fab248f8f19cbfa2f2517cc135440
YassineYOMNI <elbourachdifarida@gmail.com>
2023-06-26 07:11:44 -0700

    c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ *
+ * @str : The string to be printed.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		_putchar(str[i++]);
+
+	_putchar('\n');
+}
