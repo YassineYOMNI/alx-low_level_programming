cd17f3ff81fcbc5e0887701a0a39645bff68debe
YassineYOMNI <elbourachdifarida@gmail.com>
2023-07-27 08:44:14 -0700

    c

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,11 @@
-; File: 101-hello_holberton.asm
-; Auth: Mohammed Aitouaaziz
-; Desc: 64-bit assembly program that prints
-;	Hello, Holberton followed by a new line.
+section		.text
+	extern	printf
+	global	main
+main:
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
 
-extern printf
 
-section .data
-db "Hello, Holberton, 0
-db "%s", 10, 0
-
-section .text
-
-global main
-
-push rbp
-
-mov rdi,format
-mov rsi,message
-mov rax,0
-call printf
-
-pop rbp
-
-mov rax,0
-ret
+section 	.data
+	msg db 'Heloo, Holberton', 0xa, 0
